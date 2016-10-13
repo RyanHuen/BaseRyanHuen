d3fa58c57b5210130f8e747702e02e692a469ed7
xuanweihong <xuanweihong@phoenixos.com>
2016-10-13 14:57:03 +0800

    permission control lib update

--- app/src/com/ryanhuen/baseryanhuen/MainActivity.java
+++ app/src/com/ryanhuen/baseryanhuen/MainActivity.java
@@ -1,6 +1,10 @@
 
 package com.ryanhuen.baseryanhuen;
 
+import com.ryanhuen.permission_m.PermissionConfig;
+import com.ryanhuen.permission_m.RequestPermission;
+
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initPermission();
+    }
+
+    private void initPermission() {
+        String[] permissionList = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_PHONE_STATE
+        };
+        PermissionConfig.addToPermissionList(permissionList);
+        RequestPermission.verifyPermissions(this);
     }
 }
